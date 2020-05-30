62582cf32f29987504ec75eb81dc05d27de3fd48
alithecodeguy <alithecodeguy@gmail.com>
2020-05-30 23:47:39 +0430

    remove dist

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-// InteractiveIranMap.js
-import React from "react";
-
-const InteractiveIranMap = props => {
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "InteractiveIranMap!"));
-};
-
-export default InteractiveIranMap;
